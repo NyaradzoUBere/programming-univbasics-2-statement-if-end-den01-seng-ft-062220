7b2cd544e0e55310b4102e0f85e813b59e24713a
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-05-30 04:17:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -25,9 +25,8 @@ else
   puts "Stay home and read Hegel"
 end
 
-puts "You know what year it is?"
 this_year = 2019
-puts "Hey, it's not 2019!"
+puts "Hey, it's not 2019!" 
 unless this_year == 2019
 
 
