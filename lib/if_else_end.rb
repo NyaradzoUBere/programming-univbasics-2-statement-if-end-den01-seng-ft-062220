99548eca2c43679032e25324fce1794a8a3f596a
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-05-30 04:12:02 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,6 +5,8 @@ if run_code_inside
   puts "code inside"
 end
 puts "Code after if...end"
+
+
 chance_of_rain = 1
 puts "Let's go outside!"
 if chance_of_rain > 0.5
@@ -13,3 +15,12 @@ else
   puts "Enjoy the fine day!"
 end
 puts "Oh, and always wear sunscreen!"
+
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain < .75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel"
+end
